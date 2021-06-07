bb5dfd112a9b09a7d430956c8c542b0eca94e390
zergos1987 <85062042+zergos1987@users.noreply.github.com>
2021-06-07 18:07:08 +0300

    Update admin.py

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,93 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from rangefilter.filter import DateRangeFilter, DateTimeRangeFilter
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin, ImportExportActionModelAdmin
 
-# Register your models here.
+from .models import (
+	AuditEntry,
+    UserSession
+	)
+
+
+
+
+class AuditEntryResource(resources.ModelResource):
+
+    class Meta:
+        model = AuditEntry
+        fields = (
+            'dt', 
+            'action', 
+            'request_method', 
+            'username', 
+            'ip', 
+            'device', 
+            'browser_family', 
+            'browser_version', 
+            'os_family', 
+            'os_version',)
+
+
+class AuditEntryAdmin(ImportExportModelAdmin):
+    list_display = [
+    	'dt', 
+    	'action', 
+    	'request_method', 
+    	'username', 
+    	'ip', 
+    	'device', 
+    	'browser_family', 
+    	'browser_version', 
+    	'os_family', 
+    	'os_version',]
+
+    list_filter = (
+        'dt', 'action', #('dt', DateTimeRangeFilter)
+    )
+
+    class Media:
+        js = ('/static/admin/js/jquery.grp_timepicker.js', )
+
+    resource_class = AuditEntryResource
+admin.site.register(AuditEntry, AuditEntryAdmin)
+
+class CustomUserAdmin(UserAdmin):
+    class Media:
+        js = ['/apps/accounts/origin/js/custom_admin_script.js', '/static/admin/js/jquery.grp_timepicker.js']
+        css = {
+            'all': ('apps/accounts/origin/css/custom_admin_style.css',)
+        }   
+        #js = ['apps/accounts/origin/js/custom_admin_script.js']
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
+
+
+
+
+
+class UserSessionResource(resources.ModelResource):
+
+    class Meta:
+        model = UserSession
+        fields = (
+            'user', 
+            'session',)
+
+class UserSessionAdmin(ImportExportModelAdmin):
+    list_display = [
+        'user', 
+        'session', 'created_at', 'remove_session']
+
+    list_filter = (
+        'user', 'session', 'created_at'
+    )
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    resource_class = UserSessionResource
+
+admin.site.register(UserSession, UserSessionAdmin)
