a4b5eebd5114be4dcba2e9f66c2f1d85b71f3524
zergos1987 <85062042+zergos1987@users.noreply.github.com>
2021-06-07 16:48:01 +0300

    Update drf_serializers.py

--- custom_script_extensions/drf_serializers.py
+++ custom_script_extensions/drf_serializers.py
@@ -1,23 +1,25 @@
 from rest_framework import serializers
-from djangoanalytics.apps.examples.models import (
-	test_table_model,
+
+from apps.examples.models import (
+	test_table_model, 
 	processing_module_pipeline_user_content_history)
 
 
-class test_table_model__Serializer(serializers.ModelSerializer):
-	class Meta:
-		model = test_table_model
-		fields = ('id', 'test_field', )
-
 
 class processing_module_pipeline_user_content_history__Serializer(serializers.ModelSerializer):
-	class Meta:
-		model = processing_module_pipeline_user_content_history
-		fields = '__all__'
-		#fields = ['id', 'login', 'datetime', 'userobjectparams', 'userobjectwindow']
+    class Meta:
+        model = processing_module_pipeline_user_content_history
+        fields = '__all__'
+        #fields = ['id', "login", "datetime", "userobjectparams", "userobjectwindow"]
+
+
+class test_table_model__Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = test_table_model
+        fields = ('id', 'test_field', 'datetime_start_field', 'datetime_end_field', 'boolean_field', 'integer_field', 'integer_choice_field',)
 
 
 class Generic__Serializer(serializers.ModelSerializer):
-	class Meta:
-		model = None
-		fields = '__all__'
+    class Meta:
+        model = None
+        fields = '__all__'
