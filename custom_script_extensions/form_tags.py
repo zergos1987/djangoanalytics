3b7e9b478c7879fd2f9645244f8f476cba4a44ab
zergos1987 <85062042+zergos1987@users.noreply.github.com>
2021-06-07 18:48:16 +0300

    Update form_tags.py

--- custom_script_extensions/form_tags.py
+++ custom_script_extensions/form_tags.py
@@ -1,6 +1,15 @@
+from django.contrib.auth.models import User
 from django import template
+import six
+
+from django.core.exceptions import PermissionDenied
+from django.contrib.auth.decorators import user_passes_test
+
 
 register = template.Library()
+
+
+
 
 @register.filter
 def field_type(bound_field):
@@ -15,3 +24,25 @@ def input_class(bound_field):
         elif field_type(bound_field) != 'PasswordInput':
             css_class = 'is-valid'
     return 'form-control {}'.format(css_class)
+
+@register.filter('in_group')
+def in_group(user, group_name):
+    return user.groups.filter(name=group_name).exists()
+
+@register.filter('group_required')
+def group_required(group, raise_exception=False):
+    def check_perms(user):
+        if isinstance(group, six.string_types):
+            groups = (group, )
+        else:
+            groups = group
+        # First check if the user has the permission (even anon users)
+
+        if user.groups.filter(name__in=groups).exists():
+            return True
+        # In case the 403 handler should be called raise the exception
+        if raise_exception:
+            raise PermissionDenied
+        # As the last resort, show the login form
+        return False
+    return user_passes_test(check_perms)
