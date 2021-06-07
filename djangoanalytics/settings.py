023088960ca6b7eb2ff6d641910314555ca32025
zergos1987 <4630574@mail.ru>
2021-06-07 08:07:50 +0300

    zs_admin base 5

--- djangoanalytics/settings.py
+++ djangoanalytics/settings.py
@@ -175,6 +175,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'staticfiles',
 ]
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
