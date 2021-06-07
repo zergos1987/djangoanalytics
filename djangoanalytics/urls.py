c0f3e632fc2e6d22de6884d7d5b025a2ad9cf058
zergos1987 <85062042+zergos1987@users.noreply.github.com>
2021-06-07 18:00:08 +0300

    Update urls.py

--- djangoanalytics/urls.py
+++ djangoanalytics/urls.py
@@ -19,6 +19,11 @@ from django.conf.urls import include, url
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.contrib.auth import views as auth_views
+from apps.accounts import views as accounts_views
+
+from django.shortcuts import redirect
+
 urlpatterns = [
     path('grappelli/', include('grappelli.urls')), # grappelli URLS
     path('admin', admin.site.urls),
@@ -30,6 +35,29 @@ urlpatterns = [
     path('zs_examples/', include('apps.app_zs_examples.urls')),
     path('db_sadko/', include('apps.database_oracle_sadko.urls')),
     path('db_sqlite_test/', include('apps.database_sqlite_test.urls')),
+]
+
+urlpatterns += [
+    url(r'^accounts/signup/$', accounts_views.signup, name='signup'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+    url(r'^logout_from_admin/$', accounts_views.signout, name='signout'),
+    url(r'^reset/$', auth_views.PasswordResetView.as_view(
+        template_name='registration/password_reset.html',
+        email_template_name='registration/password_reset_email.html',
+        subject_template_name='registration/password_reset_subject.txt'), name='password_reset'),
+    url(r'^reset/done/$', auth_views.PasswordResetDoneView.as_view(
+        template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
+    url(r'^reset/complete/$', auth_views.PasswordResetCompleteView.as_view(
+        template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
+    url(r'^settings/password/$', auth_views.PasswordChangeView.as_view(
+          template_name='registration/password_change.html'),
+        name='password_change'),
+    url(r'^settings/password/done/$', auth_views.PasswordChangeDoneView.as_view(
+          template_name='registration/password_change_done.html'),
+        name='password_change_done'),
 ]
 
 if settings.DEBUG:
