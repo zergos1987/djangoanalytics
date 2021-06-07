0460cb7707c47a5f029daa91b4b0fc09c03e9c2c
zergos1987 <85062042+zergos1987@users.noreply.github.com>
2021-06-07 16:46:04 +0300

    Update database_router.py

--- custom_script_extensions/database_router.py
+++ custom_script_extensions/database_router.py
@@ -1,77 +1,40 @@
 class CheckerRouter(object):
 
 	def db_for_read(self, model, **hints):
-		if model._meta.app_label == 'examples' and model._meta.db_table == 'processing_module_pipeline_user_content_history':
+		if model._meta.app_label == 'app_zs_examples' and model._meta.db_table == 'processing_module_pipeline_user_content_history':
 			return 'dash_sadko_postgres_db'
-
-		elif model._meta.app_label == 'examples':
-			return 'test_sqlite3_db'
-
-		elif model._meta.app_label == 'op_dashboards':
-			return 'op_dashboards_sqlite3_db'
-
-		elif model._meta.app_label == 'op_surveys':
-			return 'op_surveys_sqlite3_db'
-
+		if model._meta.app_label == 'database_oracle_sadko':
+			return 'oracle_sadko_db'
+		elif model._meta.app_label == 'database_sqlite_test':
+			return 'test_remote_db'
 		return 'default'
-
-
 
 	def db_for_write(self, model, **hints):
-		if model._meta.app_label == 'examples' and model._meta.db_table == 'processing_module_pipeline_user_content_history':
+		if model._meta.app_label == 'app_zs_examples' and model._meta.db_table == 'processing_module_pipeline_user_content_history':
 			return 'dash_sadko_postgres_db'
-
-		elif model._meta.app_label == 'examples':
-			return 'test_sqlite3_db'
-
-		elif model._meta.app_label == 'op_dashboards':
-			return 'op_dashboards_sqlite3_db'
-
-		elif model._meta.app_label == 'op_surveys':
-			return 'op_surveys_sqlite3_db'
-
+		if model._meta.app_label == 'database_oracle_sadko':
+			return 'oracle_sadko_db'
+		elif model._meta.app_label == 'database_sqlite_test':
+			return 'test_remote_db'
 		return 'default'
 
-
-
 	def allow_relation(self, obj1, obj2, **hints):
-		if obj1._meta.app_label == 'dash_sadko' or obj2._meta.app_label == 'dash_sadko':
+		if obj1._meta.app_label == 'database_oracle_sadko' or obj2._meta.app_label == 'database_oracle_sadko':
 			return True
-		elif 'dash_sadko' not in [obj1._meta.app_label, obj2._meta.app_label]:
+		elif 'database_oracle_sadko' not in [obj1._meta.app_label, obj2._meta.app_label]:
 			return True
-
-		elif obj1._meta.app_label == 'examples' or obj2._meta.app_label == 'examples':
+		elif obj1._meta.app_label == 'database_sqlite_test' or obj2._meta.app_label == 'database_sqlite_test':
 			return True
-		elif 'examples' not in [obj1._meta.app_label, obj2._meta.app_label]:
+		elif 'database_sqlite_test' not in [obj1._meta.app_label, obj2._meta.app_label]:
 			return True
-
-		elif obj1._meta.app_label == 'op_dashboards' or obj2._meta.app_label == 'op_dashboards':
-			return True
-		elif 'op_dashboards' not in [obj1._meta.app_label, obj2._meta.app_label]:
-			return True
-
-		elif obj1._meta.app_label == 'op_surveys' or obj2._meta.app_label == 'op_surveys':
-			return True
-		elif 'op_surveys' not in [obj1._meta.app_label, obj2._meta.app_label]:
-			return True
-
 		return False
 
-
-
 	def allow_migrate(self, db, app_label, model_name=None, **hints):
-		if app_label == 'examples':
-			return db == 'test_sqlite3_db'
-
-		if app_label == 'op_dashboards':
-			return db == 'op_dashboards_sqlite3_db'
-
-		if app_label == 'op_surveys':
-			return db == 'op_surveys_sqlite3_db'
-
+		if app_label == 'database_oracle_sadko':
+			return db == 'oracle_sadko_db'
+		if app_label == 'database_sqlite_test':
+			return db == 'test_remote_db'
 		return None
-
-
 
 
 
