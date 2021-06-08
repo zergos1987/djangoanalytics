b77814852a908827d0228b3f009a4243097e267e
zergos1987 <85062042+zergos1987@users.noreply.github.com>
2021-06-08 15:02:11 +0300

    Update urls.py

--- apps/app_opensource_dashboards/urls.py
+++ apps/app_opensource_dashboards/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from apps.app_opensource_dashboards import views
+from django.views.generic import TemplateView
 from django.urls import path
-
 
 
 urlpatterns = [
