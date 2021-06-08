0ade235a28199be2b1f75babe3b7994033cc9db0
zergos1987 <85062042+zergos1987@users.noreply.github.com>
2021-06-08 17:05:00 +0300

    Update drf_permissions.py

--- custom_script_extensions/drf_permissions.py
+++ custom_script_extensions/drf_permissions.py
@@ -3,17 +3,17 @@ from rest_framework.permissions import BasePermission
 
 class CheckGroupPermissions__dynamic__ORM(BasePermission):
 	def has_permission(self, request, view):
-		if request.user.groups.filter(name='examples_app__genericTable__drf__get').exists() and request.method in ['GET', 'OPTIONS']:
+		if request.user.groups.filter(name='app_zs_examples__genericTable__drf__get').exists() and request.method in ['GET', 'OPTIONS']:
 			return True
-		elif request.user.groups.filter(name='examples_app__genericTable__drf__post').exists() and request.method in ['POST']:
+		elif request.user.groups.filter(name='app_zs_examples__genericTable__drf__post').exists() and request.method in ['POST']:
 			return True
-		elif request.user.groups.filter(name='examples_app__genericTable__drf__put').exists() and request.method in ['PUT', 'UPDATE']:
+		elif request.user.groups.filter(name='app_zs_examples__genericTable__drf__put').exists() and request.method in ['PUT', 'UPDATE']:
 			return True
-		elif request.user.groups.filter(name='examples_app__genericTable__drf__delete').exists() and request.method in ['DELETE']:
+		elif request.user.groups.filter(name='app_zs_examples__genericTable__drf__delete').exists() and request.method in ['DELETE']:
 			return True
-		elif request.user.groups.filter(name='examples_app__genericTable__drf__export').exists() and request.method in ['GET']:
+		elif request.user.groups.filter(name='app_zs_examples__genericTable__drf__export').exists() and request.method in ['GET']:
 			return True
-		elif request.user.groups.filter(name='examples_app__genericTable__drf__import').exists() and request.method in ['POST']:
+		elif request.user.groups.filter(name='app_zs_examples__genericTable__drf__import').exists() and request.method in ['POST']:
 			return True
 		return False
 
@@ -22,16 +22,16 @@ class CheckGroupPermissions__ORM(BasePermission):
 	def has_permission(self, request, view):
 		user_model_name = view.__class__.__name__
 		user_model_name = user_model_name[:user_model_name.find('__')]
-		if request.user.groups.filter(name=f'examples_app__{user_model_name}__drf__get').exists() and request.method in ['GET', 'OPTIONS']:
+		if request.user.groups.filter(name=f'app_zs_examples__{user_model_name}__drf__get').exists() and request.method in ['GET', 'OPTIONS']:
 			return True
-		elif request.user.groups.filter(name=f'examples_app__{user_model_name}__drf__post').exists() and request.method in ['POST']:
+		elif request.user.groups.filter(name=f'app_zs_examples__{user_model_name}__drf__post').exists() and request.method in ['POST']:
 			return True
-		elif request.user.groups.filter(name=f'examples_app__{user_model_name}__drf__put').exists() and request.method in ['PUT', 'UPDATE']:
+		elif request.user.groups.filter(name=f'app_zs_examples__{user_model_name}__drf__put').exists() and request.method in ['PUT', 'UPDATE']:
 			return True
-		elif request.user.groups.filter(name=f'examples_app__{user_model_name}__drf__delete').exists() and request.method in ['DELETE']:
+		elif request.user.groups.filter(name=f'app_zs_examples__{user_model_name}__drf__delete').exists() and request.method in ['DELETE']:
 			return True
-		elif request.user.groups.filter(name=f'examples_app__{user_model_name}__drf__export').exists() and request.method in ['GET']:
+		elif request.user.groups.filter(name=f'app_zs_examples__{user_model_name}__drf__export').exists() and request.method in ['GET']:
 			return True
-		elif request.user.groups.filter(name=f'examples_app__{user_model_name}__drf__import').exists() and request.method in ['POST']:
+		elif request.user.groups.filter(name=f'app_zs_examples__{user_model_name}__drf__import').exists() and request.method in ['POST']:
 			return True
 		return False
