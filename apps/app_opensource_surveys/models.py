7c23a7dfa0467557ed1ef6ad6df6446d073ace90
zergos1987 <85062042+zergos1987@users.noreply.github.com>
2021-06-08 15:00:38 +0300

    Update models.py

--- apps/app_opensource_surveys/models.py
+++ apps/app_opensource_surveys/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class app_opensource_surveys_test_table(models.Model):
+    test_field = models.CharField(max_length=150)
+
+    class Meta:
+    	# app_label helps django to recognize your db
+    	app_label = 'app_opensource_surveys'
+
+
+    def __str__(self):
+        return self.test_field
