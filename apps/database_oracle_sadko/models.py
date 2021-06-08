0a95d2b860d6cf44e5a67b5d7c129d02ca460d70
zergos1987 <85062042+zergos1987@users.noreply.github.com>
2021-06-08 14:52:31 +0300

    Update models.py

--- apps/database_oracle_sadko/models.py
+++ apps/database_oracle_sadko/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class database_oracle_sadko_test_table(models.Model):
+    test_field = models.CharField(max_length=150)
+
+    class Meta:
+    	# app_label helps django to recognize your db
+    	app_label = 'database_oracle_sadko'
+
+
+    def __str__(self):
+        return self.test_field
