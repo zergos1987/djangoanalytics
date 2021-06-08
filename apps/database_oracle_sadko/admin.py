9e00ef19306b91c3a6fb9e758532fe282678b51a
zergos1987 <85062042+zergos1987@users.noreply.github.com>
2021-06-08 14:53:13 +0300

    Update admin.py

--- apps/database_oracle_sadko/admin.py
+++ apps/database_oracle_sadko/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin, ImportExportActionModelAdmin
+
+from .models import (
+		database_oracle_sadko_test_table,
+	)
 
 # Register your models here.
+class database_oracle_sadko_test_tableResource(resources.ModelResource):
+
+    class Meta:
+        model = database_oracle_sadko_test_table
+        fields = (
+            'test_field',)
+
+
+class database_oracle_sadko_test_tableAdmin(ImportExportModelAdmin):
+    list_display = [
+    	'test_field',]
+
+    list_filter = (
+        'test_field',  #('dt', DateTimeRangeFilter)
+    )
+
+    class Media:
+        js = ('/static/admin/js/jquery.grp_timepicker.js', )
+
+    resource_class = database_oracle_sadko_test_tableResource
+admin.site.register(database_oracle_sadko_test_table, database_oracle_sadko_test_tableAdmin)
