e89c594bd982b7a84f5cb43bdad79ac1ef6866d1
zergos1987 <85062042+zergos1987@users.noreply.github.com>
2021-06-08 14:50:02 +0300

    Update models.py

--- apps/database_sqlite_test/models.py
+++ apps/database_sqlite_test/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class database_sqlite_test_test_table(models.Model):
+    test_field = models.CharField(max_length=150)
+
+    class Meta:
+    	# app_label helps django to recognize your db
+    	app_label = 'database_sqlite_test'
+
+
+    def __str__(self):
+        return self.test_field
