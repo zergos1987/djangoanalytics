b7413c178bac7ebacd32b8973ac2d90a23464597
zergos1987 <85062042+zergos1987@users.noreply.github.com>
2021-06-08 14:54:33 +0300

    Update models.py

--- apps/app_zs_examples/models.py
+++ apps/app_zs_examples/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class app_zs_examples_test_table(models.Model):
+    test_field = models.CharField(max_length=150)
+
+    class Meta:
+    	# app_label helps django to recognize your db
+    	app_label = 'app_zs_examples'
+
+
+    def __str__(self):
+        return self.test_field
