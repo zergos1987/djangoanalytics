7c6d1a06325d93b20735277dfe0ea0cf8ebbd67d
zergos1987 <85062042+zergos1987@users.noreply.github.com>
2021-06-08 18:57:42 +0300

    Create djangoanalytics_initialize.py

---
+++ custom_script_extensions/djangoanalytics_initialize.py
@@ -0,0 +1,14 @@
+# from django.db import connections
+# from django.db.utils import OperationalError
+
+# db_conn = connections['default']
+
+# try:
+#     c = db_conn.cursor()
+# except OperationalError:
+#     connected = False
+# else:
+#     connected = True
+
+def initialazie_base_content():
+	print('FFFFFFFFF')
