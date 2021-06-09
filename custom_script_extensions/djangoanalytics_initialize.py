c4f31c85568449961c25a96d53013c159d62b83c
zergos1987 <85062042+zergos1987@users.noreply.github.com>
2021-06-09 11:17:51 +0300

    Update djangoanalytics_initialize.py

--- custom_script_extensions/djangoanalytics_initialize.py
+++ custom_script_extensions/djangoanalytics_initialize.py
@@ -1,14 +1,32 @@
 # from django.db import connections
 # from django.db.utils import OperationalError
 
-# db_conn = connections['default']
 
-# try:
-#     c = db_conn.cursor()
-# except OperationalError:
-#     connected = False
-# else:
-#     connected = True
+
+def check_default_db_available():
+	# db_conn = connections['default']
+
+	# try:
+	# 	c = db_conn.cursor()
+	# except OperationalError:
+	# 	connected = False
+	# else:
+	# 	connected = True
+
+	return True
+
+
+def check_default_groups_exists():
+	return True, ['a', 'b']
+
 
 def initialazie_base_content():
-	print('FFFFFFFFF')
+	# check if defaut database was load
+	is_default_db_available = check_default_db_available()
+	if is_default_db_available:
+		is_default_groups_exists, group_list = check_default_groups_exists()
+
+
+	print('\ndjangoanalytics_initialize.py script output:')
+	print('   is_default_db_available:', is_default_db_available)
+	print('   is_default_groups_exists:', is_default_db_available, 'group_list:', group_list)
