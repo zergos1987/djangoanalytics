25fed8da06925a44d44ee6b9f1be595663ce88f9
zergos1987 <85062042+zergos1987@users.noreply.github.com>
2021-06-10 18:18:10 +0300

    Update urls.py

--- apps/app_zs_dashboards/urls.py
+++ apps/app_zs_dashboards/urls.py
@@ -3,6 +3,7 @@ from apps.app_zs_dashboards import views
 from django.views.generic import TemplateView
 from django.urls import path
 
+app_name = 'app_zs_dashboards'
 
 urlpatterns = [
     url(r'^$', views.index, name='index_app_zs_dashboards'),
