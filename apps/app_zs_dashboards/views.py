2a1848d05ace9e2b8cb70cac977d7bd8f4fce95b
zergos1987 <85062042+zergos1987@users.noreply.github.com>
2021-06-11 11:03:54 +0300

    Update views.py

--- apps/app_zs_dashboards/views.py
+++ apps/app_zs_dashboards/views.py
@@ -18,9 +18,9 @@ import os
 
 
 # Create your views here.
-#@method_decorator([login_required, permission_required("app_zs_dashboards.can_view_app")], name="dispatch")
+#@method_decorator([login_required, permission_required("app_zs_dashboards.view_app")], name="dispatch")
 @login_required
-@permission_required('app_zs_dashboards.can_view_app')
+@permission_required('app_zs_dashboards.view_app')
 def index(request):
 
 	template = 'app_zs_dashboards/index.html'
