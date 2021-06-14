ce93ed9a8cfa767c6869bd84895f1f4aee268184
zergos1987 <85062042+zergos1987@users.noreply.github.com>
2021-06-14 15:54:57 +0300

    Update admin.py

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
 from rangefilter.filter import DateRangeFilter, DateTimeRangeFilter
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin, ImportExportActionModelAdmin
-from django.contrib.auth.models import Permission
+from django.contrib.auth.models import User, Group, Permission, ContentType
 from django.contrib.sessions.models import Session
 from django.utils.translation import ugettext_lazy as _
 
@@ -16,85 +15,6 @@ from .models import (
 
 
 # Register your models here.
-admin.site.register(Permission)
-
-class SessionAdmin(admin.ModelAdmin):
-    def _session_data(self, obj):
-        return obj #pprint.pformat(obj.get_decoded()).replace('\n', '<br>\n')
-    _session_data.allow_tags=True
-    list_display = ['session_key', '_session_data', 'expire_date']
-    readonly_fields = ['_session_data']
-    exclude = ['session_data']
-    date_hierarchy='expire_date'
-admin.site.register(Session, SessionAdmin)
-
-
-class appResource(resources.ModelResource):
-
-    class Meta:
-        model = app
-        fields = (
-            'test_field',)
-
-
-class appAdmin(ImportExportModelAdmin):
-    list_display = [
-        'test_field',]
-
-    list_filter = (
-        'test_field',  #('dt', DateTimeRangeFilter)
-    )
-
-    class Media:
-        js = ('/static/admin/js/jquery.grp_timepicker.js', )
-
-    resource_class = appResource
-admin.site.register(app, appAdmin)
-
-
-
-class AuditEntryResource(resources.ModelResource):
-
-    class Meta:
-        model = AuditEntry
-        fields = (
-            'dt', 
-            'action', 
-            'request_method', 
-            'username', 
-            'ip', 
-            'device', 
-            'browser_family', 
-            'browser_version', 
-            'os_family', 
-            'os_version',)
-
-	
-class AuditEntryAdmin(ImportExportModelAdmin):
-    list_display = [
-    	'dt', 
-    	'action', 
-    	'request_method', 
-    	'username', 
-    	'ip', 
-    	'device', 
-    	'browser_family', 
-    	'browser_version', 
-    	'os_family', 
-    	'os_version',]
-
-    list_filter = (
-        'dt', 'action', #('dt', DateTimeRangeFilter)
-    )
-
-    class Media:
-        js = ('/static/admin/js/jquery.grp_timepicker.js', )
-
-    resource_class = AuditEntryResource
-admin.site.register(AuditEntry, AuditEntryAdmin)
-
-
-
 class CustomUserAdmin(UserAdmin):
 
     model = User
@@ -141,60 +61,169 @@ class CustomUserAdmin(UserAdmin):
             return response
         else:
             return super(CustomUserAdmin, self).change_view(request, object_id, form_url, extra_context, *args, **kwargs)
-    # readonly_fields = [
-    #     'date_joined',
-    #     'last_login',
-    # ]
-
-    # def get_form(self, request, obj=None, **kwargs):
-    #     form = super().get_form(request, obj, **kwargs)
-    #     is_superuser = request.user.is_superuser
-    #     disabled_fields = set()  # type: Set[str]
-
-    #     if not is_superuser:
-    #         disabled_fields |= {
-    #             'username',
-    #             'is_staff',
-    #             'is_superuser',
-    #         }
-
-    #     for f in disabled_fields:
-    #         if f in form.base_fields:
-    #             form.base_fields[f].disabled = True
-
-    #     return form
-
-    class Media:
-        js = ['/apps/accounts/origin/js/custom_admin_script.js', '/static/admin/js/jquery.grp_timepicker.js']
-        css = {
-            'all': ('apps/accounts/origin/css/custom_admin_style.css',)
-        }   
-        #js = ['apps/accounts/origin/js/custom_admin_script.js']
 
 admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
 
 
 
-class UserSessionResource(resources.ModelResource):
 
+
+
+print('Session', [field.name for field in Session._meta.get_fields()])
+print('Group', [field.name for field in Group._meta.get_fields()])
+print('Permission', [field.name for field in Permission._meta.get_fields()])
+print('UserSession', [field.name for field in UserSession._meta.get_fields()])
+
+
+
+class app_Resource(resources.ModelResource):
     class Meta:
-        model = UserSession
-        fields = (
-            'user', 
-            'session',)
+        model = app
+        fields = ('id',)
 
-class UserSessionAdmin(ImportExportModelAdmin):
-    list_display = [
-        'user', 
-        'session', 'created_at', 'remove_session']
 
-    list_filter = (
-        'user', 'session', 'created_at'
-    )
+class app_Admin(ImportExportModelAdmin):
+    list_display = ['id']
+    list_filter = ('id', )
 
     def has_add_permission(self, request, obj=None):
         return False
 
-    resource_class = UserSessionResource
-admin.site.register(UserSession, UserSessionAdmin)
+    resource_class = app_Resource
+
+admin.site.register(app, app_Admin)
+
+
+class Permission_Resource(resources.ModelResource):
+    class Meta:
+        model = Permission
+        fields = ('id', 'content_type', 'name', 'codename',)
+
+
+class Permission_Admin(ImportExportModelAdmin):
+
+
+class Permission_Admin(ImportExportModelAdmin):
+    def content_type_app_model_id(self, obj):
+        return obj.content_type.app_label + ' | ' + obj.content_type.model + ' | ' + str(obj.content_type.id)
+    content_type_app_model_id.short_description  = 'APP | Model | id'
+
+    list_display = ('id', 'content_type_app_model_id', 'name', 'codename', )
+    list_filter = ('id', 'content_type', 'name', 'codename', )
+    search_fields = ['name', 'codename', 'content_type__app_label', 'content_type__model']
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    resource_class = Permission_Resource
+
+admin.site.register(Permission, Permission_Admin)
+
+
+
+
+class Group_Resource(resources.ModelResource):
+    class Meta:
+        model = Group
+        fields = ('id', 'name',)
+
+
+class Group_Admin(ImportExportModelAdmin):
+    
+    list_display = ['id', 'name']
+    list_filter = ('id', 'name',)
+    search_fields = ['name',]
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    resource_class = Group_Resource
+
+admin.site.unregister(Group)
+admin.site.register(Group, Group_Admin)
+
+
+
+
+class Session_Resource(resources.ModelResource):
+    class Meta:
+        model = Session
+        fields = ('session_key', 'expire_date', )
+
+
+class Session_Admin(ImportExportModelAdmin):
+    
+    list_display = ['session_key', 'expire_date']
+    list_filter = ('session_key', 'expire_date', )
+    exclude = ['session_data']
+    date_hierarchy='expire_date'
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    resource_class = Session_Resource
+
+admin.site.register(Session, Session_Admin)
+
+
+
+
+class UserSession_Resource(resources.ModelResource):
+    class Meta:
+        model = UserSession
+        fields = ('user', 'session',)
+
+
+class UserSession_Admin(ImportExportModelAdmin):
+    list_display = ['user', 'session', 'created_at', 'remove_session']
+    list_filter = ('user', 'session', 'created_at',)
+    #search_fields = ['user', 'session', 'created_at',]
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    resource_class = UserSession_Resource
+
+admin.site.register(UserSession, UserSession_Admin)
+
+
+
+
+class AuditEntry_Resource(resources.ModelResource):
+    class Meta:
+        model = AuditEntry
+        fields = (
+            'dt', 
+            'action', 
+            'request_method', 
+            'username', 
+            'ip', 
+            'device', 
+            'browser_family', 
+            'browser_version', 
+            'os_family', 
+            'os_version',)
+
+
+class AuditEntry_Admin(ImportExportModelAdmin):
+    list_display = [
+        'dt', 
+        'action', 
+        'request_method', 
+        'username', 
+        'ip', 
+        'device', 
+        'browser_family', 
+        'browser_version', 
+        'os_family', 
+        'os_version',]
+    list_filter = ('dt', 'action', 'request_method', 'request_method',)
+    search_fields = ['action', 'request_method', 'username', 'ip', 'device', 'browser_family', 'browser_version', 'os_family', 'os_version',]
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    resource_class = AuditEntry_Resource
+
+admin.site.register(AuditEntry, AuditEntry_Admin)
