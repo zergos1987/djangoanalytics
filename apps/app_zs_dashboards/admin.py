1cffcc6b901fe0c1818efb4053d90748f79b6f9d
zergos1987 <85062042+zergos1987@users.noreply.github.com>
2021-06-18 16:42:59 +0300

    Update admin.py

--- apps/app_zs_dashboards/admin.py
+++ apps/app_zs_dashboards/admin.py
@@ -22,6 +22,19 @@ class appAdmin(ImportExportModelAdmin):
     list_filter = (
         'test_field',  #('dt', DateTimeRangeFilter)
     )
+    
+    def has_import_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
+        else:
+            return False
+
+    def has_export_permission(self, request, obj=None):
+        return True
+        # if request.user.is_superuser:
+        #     return True
+        # else:
+        #     return False
 
     class Media:
         js = ('/static/admin/js/jquery.grp_timepicker.js', )
