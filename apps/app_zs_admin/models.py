94a5c1b3d805c40ac9207ddd0bdb0636d3d28d19
zergos1987 <85062042+zergos1987@users.noreply.github.com>
2021-06-24 12:21:23 +0300

    Update models.py

--- apps/app_zs_admin/models.py
+++ apps/app_zs_admin/models.py
@@ -1,16 +1,25 @@
 from django.db import models
-
+from colorfield.fields import ColorField
+from jsonfield import JSONField
 
 # Create your models here.
 
-
 class app(models.Model):
-    test_field = models.CharField(max_length=150)
+	app_brand_name = models.CharField(max_length=400, null=True)
+	app_brand_color = ColorField(default='#333')
+	app_brand_ico = models.ImageField(upload_to='img', null=True)
+	app_brand_logo = models.ImageField(upload_to='img', null=True)
+	app_settings = JSONField(default=dict)
+	is_actual = models.BooleanField(default=False)
 
-    class Meta:
-    	# app_label helps django to recognize your db
-    	app_label = 'app_zs_admin'
+	class Meta:
+		# app_label helps django to recognize your db
+		app_label = 'app_zs_admin'
 
 
-    def __str__(self):
-        return self.test_field
+	def __str__(self):
+		field_values = []
+		for field in self._meta.get_fields():
+			field_values.append(str(getattr(self, field.name, '')))
+		field_values = ['app_brand_name', 'is_actual',]
+		return ' '.join(field_values)
