161a7e94f902ae9daba45f72629fb380164648a1
zergos1987 <85062042+zergos1987@users.noreply.github.com>
2021-06-24 13:47:22 +0300

    Update views.py

--- apps/app_zs_admin/views.py
+++ apps/app_zs_admin/views.py
@@ -1,20 +1,10 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404, redirect
-from django.http import HttpResponse, FileResponse, Http404, HttpResponseRedirect, HttpResponseForbidden, HttpResponsePermanentRedirect
-from django.contrib.auth.models import User, Group
-from django.views.generic import UpdateView, ListView, TemplateView, RedirectView
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
 from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse, FileResponse, Http404, HttpResponseRedirect, HttpResponseForbidden, HttpResponsePermanentRedirect
 
-from django.db.models import Case, Sum, Min, Max, Count, When, Q, F, Value, IntegerField, CharField, DateField, DateTimeField
+from .models import app
 
-import uuid
-from django.utils.crypto import get_random_string
-
-from datetime import datetime
-import json
-
-import os
 
 
 # Create your views here.
@@ -23,10 +13,7 @@ import os
 @permission_required('app_zs_admin.view_app')
 def index(request):
 
+	app_settings = app.objects.filter(is_actual=True).first()
 	template = 'app_zs_admin/index.html'
-	breadcrumb_nav_active = {
-		"breadcrumb_nav_active": "Home"
-	}
 
-	return render(request, template, breadcrumb_nav_active)
-
+	return render(request, template, app_settings)
