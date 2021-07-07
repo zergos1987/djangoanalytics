e174e839f6f2c2c2a85486b674e69af274db4c3c
zergos1987 <85062042+zergos1987@users.noreply.github.com>
2021-07-07 14:58:40 +0300

    Update forms.py

--- custom_script_extensions/forms.py
+++ custom_script_extensions/forms.py
@@ -11,12 +11,3 @@ class SignUpForm(UserCreationForm):
 
     def __init__(self, *args, **kwargs):
     	super(SignUpForm, self).__init__(*args, **kwargs)
-
-    	for fieldname in ['username', 'email', 'password1', 'password2']:
-    		self.fields[fieldname].help_text = self.fields[fieldname].help_text.replace(
-    			"Your password can’t be too similar to your other personal information.", 
-    			"Ваш пароль не должен быть схожим с другой персональной информацией.").replace(
-    			"Your password can’t be a commonly used password.",
-    			"Ваш пароль не должен быть распространенным.").replace(
-    			"Your password can’t be entirely numeric.",
-    			"Ваш пароль не может состоять только из цифр.")
