f08ac9d32ccae286db1517a90075f432d7594150
zergos1987 <85062042+zergos1987@users.noreply.github.com>
2021-07-12 13:10:23 +0300

    Update admin.py

--- apps/app_zs_admin/admin.py
+++ apps/app_zs_admin/admin.py
@@ -235,11 +235,11 @@ class aside_left_menu_includesResource(resources.ModelResource):
     class Meta:
         model = aside_left_menu_includes
         fields = (
-            'name',  'menu_level', 'menu_icon_type', 'render_app_name',  'parent_name_order_by', 'name_order_by',  'is_actual',  'href',  ) 
+            'name',  'menu_level', 'menu_icon_type',  'parent_name_order_by', 'name_order_by', 'render_app_name', 'href', 'is_actual', ) 
 
 class aside_left_menu_includesAdmin(ImportExportModelAdmin):
     list_display = [
-        'id', 'parent_name_short', 'name',  'menu_level', 'menu_icon_type', 'render_app_name',  'parent_name_order_by', 'name_order_by',  'is_actual',  'href']
+        'id', 'parent_name_short', 'name',  'menu_level', 'menu_icon_type', 'parent_name_order_by', 'name_order_by', 'render_app_name', 'href', 'is_actual']
 
     list_filter = (
         'name',  'is_actual',  #('dt', DateTimeRangeFilter)
