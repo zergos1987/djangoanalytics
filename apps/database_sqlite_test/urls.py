7ada9ca6a5a5d6974ebb48be15fb399bb68a62cc
zergos1987 <85062042+zergos1987@users.noreply.github.com>
2021-07-22 16:14:26 +0300

    Update urls.py

--- apps/database_sqlite_test/urls.py
+++ apps/database_sqlite_test/urls.py
@@ -3,7 +3,7 @@ from apps.database_sqlite_test import views
 from django.views.generic import TemplateView
 from django.urls import path
 
-app_name = 'database_sqlite_test'
+app_name = 'db_sqlite_test'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
