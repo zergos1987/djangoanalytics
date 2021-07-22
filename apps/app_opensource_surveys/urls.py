7294a46700a5b6e763b019c58b9c5ad90ef597e7
zergos1987 <85062042+zergos1987@users.noreply.github.com>
2021-07-22 16:13:00 +0300

    Update urls.py

--- apps/app_opensource_surveys/urls.py
+++ apps/app_opensource_surveys/urls.py
@@ -3,7 +3,7 @@ from apps.app_opensource_surveys import views
 from django.views.generic import TemplateView
 from django.urls import path
 
-app_name = 'app_opensource_surveys'
+app_name = 'os_surveys'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
