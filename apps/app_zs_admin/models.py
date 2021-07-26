86676aeb3dc776203189548e8a38d445a4eb3bcf
zergos1987 <85062042+zergos1987@users.noreply.github.com>
2021-07-26 12:11:14 +0300

    Update models.py

--- apps/app_zs_admin/models.py
+++ apps/app_zs_admin/models.py
@@ -118,7 +118,7 @@ class aside_left_menu_includes(models.Model):
 	url_access_via_users = models.ManyToManyField(User, blank=True)
 	render_app_name  = models.CharField(max_length=70, choices=app_name_choices, null=True, blank=True)
 	source_app_name  = models.CharField(max_length=70, choices=app_name_choices, null=True, blank=True)
-	render_app_name_translate = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='for_render_app_name_translate', limit_choices_to={'menu_level': "level-menu"}) 
+	source_app_name_translate = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='for_render_app_name_translate', limit_choices_to={'menu_level': "level-menu"}) 
 	href = models.CharField(max_length=800, blank=True, null=True, default="#")
 	external_href = models.TextField(blank=True, null=True, default="#")
 	source_type = models.CharField(max_length=20, choices=source_type_choices, default='external')
