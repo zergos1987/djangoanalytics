0560dbb866e9652717997871ca13ee3c7858b2e1
zergos1987 <85062042+zergos1987@users.noreply.github.com>
2021-07-27 15:27:01 +0300

    Update views.py

--- apps/app_opensource_dashboards/views.py
+++ apps/app_opensource_dashboards/views.py
@@ -4,6 +4,7 @@ from django.utils.decorators import method_decorator
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse, FileResponse, Http404, HttpResponseRedirect, HttpResponseForbidden, HttpResponsePermanentRedirect
 from django.urls import reverse
+import os
 
 from apps.app_zs_admin.models import app, aside_left_menu_includes
 from custom_script_extensions.custom_permissions_check import check_user_content_request_permission
