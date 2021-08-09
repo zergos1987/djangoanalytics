2438233bf217e778693a2bec51de402239db62a7
zergos1987 <4630574@mail.ru>
2021-08-09 07:31:58 +0300

    09082021

---
+++ metabase/service.bat
@@ -0,0 +1,11 @@
+rem set MB_SITE_NAME=DjangoAnalytics
+rem set MB_SITE_URL="https://djangoanalytics.ru"
+set MB_EMBEDDING_APP_ORIGIN=http://djangoanalytics.ru:8000
+set MB_APPLICATION_FAVICON_URL=http://djangoanalytics.ru:8000/media/img/GPB_favicon.ico
+set MB_JETTY_HOST=djangoanalytics.ru
+set MB_JETTY_PORT=8100
+set MB_JETTY_SSL=true
+rem set MB_JETTY_SSL_PORT=8100
+rem set MB_JETTY_SSL_KEYSTORE=keystore.jks
+rem set MB_JETTY_SSL_KEYSTORE_PASSWORD=368696
+java -jar metabase.jar
