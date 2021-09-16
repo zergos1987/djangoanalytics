761c4748324d3af4c96af5e6b1d88d9e25a2a4dc
zergos1987 <85062042+zergos1987@users.noreply.github.com>
2021-09-16 18:04:54 +0300

    Update models.py

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -77,7 +77,9 @@ class user_extra_details(models.Model):
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
-    user_extra_details.objects.get_or_create(user=instance)
+    if user_extra_details.objects.filter(user=instance).count() == 0:
+        instance.is_active = False
+        user_extra_details.objects.get_or_create(user=instance)
 
 # @receiver(post_save, sender=User)
 # def save_user_profile(sender, instance, **kwargs):
