0b93124f3d9230e17fbece525a9512f81301cc3a
zergos1987 <4630574@mail.ru>
2021-09-27 00:32:36 +0300

    27.09.2921

--- apps/app_zs_admin/static/app_zs_admin/origin/js/render_view.js
+++ apps/app_zs_admin/static/app_zs_admin/origin/js/render_view.js
@@ -19,3 +19,43 @@ if($('#external-container').contents().find('h1:contains("Empty content frame.")
 
 setTimeout(removeOnloadFrame_type2, 1000);
 setTimeout(removeOnloadFrame_type3, 2000);
+
+
+// load print button
+$('[data-tooltip="Поиск"]').off('click').click(function() {	
+	$('.full-screen-button').off('click').click();
+	$('.full-screen-button').hide();
+	function waitResizeState() {
+		// mb print landscape
+		if ($('.external-container.mb').length > 0) {
+			$('.render_view').css('padding-left', '0px');
+			$('.external-container.mb').css('height', 'calc(100vh + 120px)');
+			var css = '@page { size: landscape; }',
+			head = document.head || document.getElementsByTagName('head')[0],
+			style = document.createElement('style');
+
+			style.type = 'text/css';
+			style.media = 'print';
+
+			if (style.styleSheet){
+			  style.styleSheet.cssText = css;
+			} else {
+			  style.appendChild(document.createTextNode(css));
+			}
+
+			head.appendChild(style);
+		} else {
+			$('.render_view').css('margin-left', '0px');
+		}
+		window.print();
+		if ($('.external-container.mb').length > 0) {
+			$('.render_view').css('padding-left', '');
+			$('.external-container.mb').css('height', '');
+		} else {
+			$('.render_view').css('margin-left', '')
+		}
+		$('.full-screen-button').show();
+		$('.full-screen-button').off('click').click();
+	}
+	setTimeout(waitResizeState, 1000);
+})
