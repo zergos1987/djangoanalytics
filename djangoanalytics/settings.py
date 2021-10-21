3725126f4924d2bd0af3e1e132e4237d408ed789
zergos1987 <85062042+zergos1987@users.noreply.github.com>
2021-10-21 14:29:56 +0300

    Update settings.py

--- djangoanalytics/settings.py
+++ djangoanalytics/settings.py
@@ -564,7 +564,7 @@ USE_I18N = True
 USE_L10N = True
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'mediafiles'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
